ac9ad5262e2101872d49fdc75f82ade649437cfa
Fayche <1751162157@qq.com>
2024-10-28 12:58:08 +0800

    feat: Update style_transfer api path

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import styleTransferRouter from "./style_transfer";
 
 const router = Router();
+
+router.use("/style_transfer", styleTransferRouter);
 
 export default router;
