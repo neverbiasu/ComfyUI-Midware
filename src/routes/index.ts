537c23c0f65a3f6007064d2defbe9a79ac984bc2
Fayche <1751162157@qq.com>
2024-12-28 18:18:53 +0800

    chore: Add text_to_scene router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import styleTransferRouter from "./style_transfer";
 import textToPortraitRouter from "./text_to_portrait";
+import textToSceneRouter from "./text_to_scene";
 
 const router = Router();
 
 router.use("/style_transfer", styleTransferRouter);
 router.use("/text_to_portrait", textToPortraitRouter);
+router.use("/text_to_scene", textToSceneRouter);
 
 export default router;
